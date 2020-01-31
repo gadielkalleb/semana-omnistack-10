00bea170dbfb7a53d90aab0f47420b53c9506196
gadielkalleb <gadiel_kalleb@hotmail.com>
2020-01-31 00:39:51 -0300

    inicio da semana omnistack

--- web/src/App.js
+++ web/src/App.js
@@ -3,7 +3,22 @@ import React from 'react';
 function App() {
   return (
     <div id="app">
-      <aside></aside>
+      <aside>
+        
+        <strong>Cadastrar</strong>
+
+        <div className="input-block">
+          <label htmlFor="github_username">Usuario do github</label>
+          <input name="github_username" id="github_username" type="text" required />
+        </div>
+
+        <div className="input-block"></div>
+
+        <div className="input-group"></div>
+
+        <div className="input-group"></div>
+
+      </aside>
       <main></main>
     </div>
   );
